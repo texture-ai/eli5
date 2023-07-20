bcd2cbf66d601f700eb988edb541e03cc8917907
Cole Robertson <cbjrobertson@gmail.com>
2023-07-19 17:28:01 -0700

    Fix missing sampler_params attribute

--- eli5/lime/samplers.py
+++ eli5/lime/samplers.py
@@ -135,6 +135,7 @@ class MaskingTextSamplers(BaseSampler):
         self.rng_ = check_random_state(random_state)
         self.token_pattern = token_pattern
         self.samplers = list(map(self._create_sampler, sampler_params))
+        self.sampler_params = sampler_params
         if weights is None:
             self.weights = np.ones(len(self.samplers))
         else:
